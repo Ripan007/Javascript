bfc9d97706b709df0fbff53dce2689b941d32f0b
ripan <reponmondal007@gmail.com>
2024-04-07 14:41:41 +0000

    Apr 7, 2024, 8:11 PM

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function reverse(str){
-  if(!str || str.length <2 || typeof str !==  'string'){
-    return   'hmm that  is not good'
+function reverse(str) {
+  if (!str || str.length < 2 || typeof str !== 'string') {
+    return 'hmm that  is not good'
   }
-  const backwards = [];
-const totalItems  = str.length -1;
-for(let i=totalItems;i  >=0;i--){
-backwards.push(str[i])
-}
-console.log(backwards)
-return backwards.join('');
+  const backwards = []
+  const totalItems = str.length - 1
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i])
+  }
+  console.log(backwards)
+  return backwards.join('')
 }
 
 reverse('hi my name is ripan')
