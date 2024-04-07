30ea48f159881cdb025474641dec0747b6898a97
ripan <reponmondal007@gmail.com>
2024-04-07 14:40:26 +0000

    Apr 7, 2024, 8:10 PM

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function reverse(str){
   }
   const backwards = [];
 const totalItems  = str.length -1;
-for(let i=totalItems;i  >=0;i++){
+for(let i=totalItems;i  >=0;i--){
 backwards.push(str[i])
 }
 console.log(backwards)
