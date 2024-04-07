510c15820be1b88c267fb20f6fb2bbe22ce2ac7e
ripan <reponmondal007@gmail.com>
2024-04-07 06:25:31 +0000

    Apr 7, 2024, 11:55 AM

--- script.js
+++ script.js
@@ -17,6 +17,18 @@ delete this.data[this.length -1]
 this.length --;
 return lastItem
 }
+delete(index){
+const item =this.data[index];
+this.shiftItems(index)
+
+}
+shiftItems(index){
+for(let i=0;i<this.length  -1;i++){
+
+}
+this.data[i] =
+
+}
 }
 
 const newArray = new MyArray()
