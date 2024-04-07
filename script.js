6ca5bbff4b0ec53f7e483bf02703c3480509f2fd
ripan <reponmondal007@gmail.com>
2024-04-07 06:13:42 +0000

    Apr 7, 2024, 11:43 AM

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ this.data = {}
 get(index){
 return this.data[index]
 }
+push(item){
+this.data[this.length ] = item;
+this.length++;
+return   this.length
+}
 }
 
 const newArray = new MyArray()
-console.log(newArray.get('1'))
