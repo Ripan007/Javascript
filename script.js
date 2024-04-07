c923facdcd7927ae5a0f9afc32339314004aba4b
ripan <reponmondal007@gmail.com>
2024-04-07 15:54:22 +0000

    Apr 7, 2024, 9:24 PM

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function reverse(str) {
-  if (!str || str.length < 2 || typeof str !== 'string') {
-    console.log('hmm that is not good')
-  }
-  const backward = []
-  const totalItems = str.length - 1
-  for (let i = totalItems; i >= 0; i--) {
-    backward.push(str[i])
-  }
-  console.log(backward)
-  return backward.join('')
-}
+let name = ['i', 'am', 'ripan']
 
-reverse('i am ripan')
+console.log(name.join(''))
