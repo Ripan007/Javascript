37abc7d0e84f0fd6f9219941c766e522943073a3
ripan <reponmondal007@gmail.com>
2024-04-08 15:29:22 +0000

    Apr 8, 2024, 8:59 PM

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function mergeSortedArray(array1, array2) {
   let array1Item = array1[0]
   let array2Item = array2[0]
   let i = 1
+  let j = 1
 
   // check input
   if (array1.length === 0) {
@@ -12,9 +13,10 @@ function mergeSortedArray(array1, array2) {
     return array1
   }
   while (array1Item || array2Item) {
-    if (array1Item < array2Item) {
+    console.log(array1Item, array2Item)
+    if (!array2Item || array1Item < array2Item) {
       mergedArray.push(array1Item)
-      array1Item = array1[zi]
+      array1Item = array1[i]
       i++
     } else {
       mergedArray.push(array2Item)
