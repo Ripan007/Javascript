8c35b7515cb6d420d4c2af8ffef2c15ca13884a1
ripan <reponmondal007@gmail.com>
2024-04-08 03:27:44 +0000

    Apr 8, 2024, 8:57 AM

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-function reverse() {}
+function mergeSortedArray(array1, array2) {
+  const mergedArray = []
+  let array1Item = array1[0]
+  let array2Item = array2[0]
+  // check input
+  if (array1.length === 0) {
+    return array2
+  }
+  if (array2.length === 0) {
+    return array1
+  }
+  return mergedArray
+}
+
+mergeSortedArray([0, 3, 4, 31], [4, 6, 30])
