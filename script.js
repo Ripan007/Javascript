5dcdd9b5549d7fde9adee85561458e2d299572c0
ripan <reponmondal007@gmail.com>
2024-04-09 15:23:44 +0000

    Apr 9, 2024, 8:53 PM

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function twoSum(array, target) {
   for (let i = 0; i < array.length - 1; i++) {
+    console.log('i', i)
     for (let j = i + 1; j < array.length; j++) {
+      console.log('j', j)
       if (array[i] + array[j] === target) {
         return [i, j]
       }
