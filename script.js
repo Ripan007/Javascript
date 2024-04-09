9ed1e3ee8911622547f5d7b301cfb1efd336df3c
ripan <reponmondal007@gmail.com>
2024-04-09 15:57:39 +0000

    Apr 9, 2024, 9:27 PM

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log(Number.MAX_VALUE)
+function maxSumArray(array) {
+  let ans = Number.MIN_VALUE
+  console.log(ans)
+  let sum = 0
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i]
+    if (sum > ans) {
+      ans = sum
+    }
+    if (sum < 0) {
+      ans = 0
+    }
+  }
+  return ans
+}
+
+const result = maxSumArray([1, 2, 4])
+console.log(result)
