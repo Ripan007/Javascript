19044e95a016389f425dfe735f99baae6cf00f1a
ripan <reponmondal007@gmail.com>
2024-04-09 15:47:26 +0000

    Apr 9, 2024, 9:17 PM

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function maxSubArray(array) {
-  let fullLength = array.lenth - 1
-  for (let i = 0; i < fullLength; i++) {
-    console.log(i)
-    for (let j = i + 1; j < fullLength; j++) {
-      console.log(j)
-      return [array[i] + array[j]]
-    }
-  }
-}
-
-const result = maxSubArray([1, 13, 3, 6])
-console.log(result)
+let sum = Integer.Min
