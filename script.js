c72687b6580ac961d43d1072c526fd594d75e39a
ripan <reponmondal007@gmail.com>
2024-04-10 04:46:06 +0000

    Apr 10, 2024, 10:16 AM

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 class HashTable {
   constructor(size) {
-    this.data = new Array(size)[['grapes', 1000]]
+    // this.data = new Array(size)[['grapes', 1000]]
   }
 
   _hash(key) {
     let hash = 0
     for (let i = 0; i < key.length; i++) {
-      hash = (hash.key.charCodeAt(i) * i) % this.data.length
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length
     }
     return hash
   }
 }
 
 const myHashTable = new HashTable(50)
-myHashTable.set('grapes', 10000)
-myHashTable.get('grapes')
+// myHashTable.set('grapes', 10000)
+// myHashTable.get('grapes')
+myHashTable._hash('grapes')
