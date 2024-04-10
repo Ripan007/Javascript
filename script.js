11e1cb36761a394303cf88846e272bd35b8e9edc
ripan <reponmondal007@gmail.com>
2024-04-10 01:27:07 +0000

    Apr 10, 2024, 6:57 AM

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-function returnDuplicate(array) {
-  const len = array.length
-  console.log('length', len)
-  for (let i = 0; i < len - 1; i++) {
-    console.log(`mentino ${i}`)
-    for (let j = i + 1; j < len; j++) {
-      if (array[i] === array[j]) {
-        return true
-      }
-    }
-  }
-  return false
-}
-
-const result = returnDuplicate([1, 2, 3, 10])
-console.log(result)
+console.log('welcome')
