bf06b135cafd8bd099282eca60b62e5666677d13
ripan <reponmondal007@gmail.com>
2024-04-11 15:46:28 +0000

    Apr 11, 2024, 9:16 PM

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function sum(num1, num2) {
+  console.log('num1', num1)
   let count = 0
   for (let i = 0; i < num1.length; i++) {
+    console.log('i', i)
     for (let j = 0; j < num2.length; j++) {
+      console.log('j', j)
       count[(i, j)] = num1[(i, j)] + num2[(i, j)]
+      return count
     }
-    return count
   }
 }
 
-console.log(sum([12], [13]))
+console.log(sum[12], [13])
