4b5100f939413c85b3a9d5b45d1f64be5838d260
ripan <reponmondal007@gmail.com>
2024-04-12 10:34:47 +0000

    Apr 12, 2024, 4:04 PM

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function x() {
   for (let i = 1; i <= 5; i++) {
-    function close() {
-      setTimeout(() => {}, x * 2000)
+    function close(x) {
+      setTimeout((x) => {
+        console.log(x)
+      }, x * 2000)
     }
-    close()
+    close(i)
   }
 }
+
+x()
