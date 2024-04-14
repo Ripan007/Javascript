72810154bcf912f52506d65a347207a5daab3b09
ripan <reponmondal007@gmail.com>
2024-04-14 06:35:45 +0000

    Apr 14, 2024, 12:05 PM

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ const calculateArea = function (radius) {
   }
   return output
 }
+
+console.log(calculateArea(radius))
+
+const calculateCircumference = function (radius) {
+  const output = []
+  for (let i = 0; i < radius.length; i++) {
+    output.push(2 * Math.PI * radius[i])
+  }
+  return output
+}
+console.log(calculateCircumference(radius))
