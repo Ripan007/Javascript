3a696bdb46d74d48d62da35bc5fa9c3b9f80681b
ripan <reponmondal007@gmail.com>
2024-04-14 04:09:06 +0000

    Apr 14, 2024, 9:39 AM

--- script.js
+++
@@ -1,8 +0,0 @@
-function attachEventListener() {
-  let count = 0
-  document.getElementById('clickMe').addEventListener('click', function xyz() {
-    console.log(++count)
-  })
-}
-
-attachEventListener()
