29f45e0083648a905c80ede72f09772b549416c0
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-15 15:28:15 +0000

    May 15, 2024, 8:58 PM

--- script.js
+++
@@ -1,8 +0,0 @@
-console.log('test start')
-setTimeout(() => console.log('0 second timer'), 0)
-Promise.resolve('resolved promised 1').then(res => console.log(res))
-Promise.resolve('resolved primised 2').then(res => {
-    for (let i = 0; i < 10000000; i++) {
-        console.log(res)
-    }
-})
