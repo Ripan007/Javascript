93e100b66b5bcc7add4dec8f6159f4a3647ec3aa
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-15 16:28:01 +0000

    May 15, 2024, 9:58 PM

--- index.js
+++ index.js
@@ -4,28 +4,7 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
     document
         .querySelectorAll('.drum')
         [i].addEventListener('click', function () {
-            alert('button got clicked')
+            var video = new Audio('./sounds/crash.mp3')
+            video.play()
         })
 }
-
-function add(num1, num2) {
-    return num1 + num2
-}
-
-function subtract(num1, num2) {
-    return num1 - num2
-}
-
-function multiply(num1, num2) {
-    return num1 * num2
-}
-
-function division(num1, num2) {
-    return num1 / num2
-}
-
-function calculator(num1, num2, operator) {
-    return operator(num1, num2)
-}
-
-calculator(2, 3, add)
