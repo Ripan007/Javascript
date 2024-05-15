41e26909a68b75d3dbfcadbc9c7ec2ed86241baa
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-15 12:56:18 +0000

    May 15, 2024, 6:26 PM

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const randomNumber1 = Math.floor(Math.random() * 6) + 1
 const randomDiceImage = 'dice' + randomNumber1 + '.png'
 const randomImageSource = './images/' + randomDiceImage
 
+// this one is for  first image
 let image1 = document.querySelectorAll('img')[0]
 image1.setAttribute('src', randomImageSource)
+
+//  this one is for second image
+
+let image2 = document.querySelectorAll('img')[1]
+image2.setAttribute('src', randomImageSource)
