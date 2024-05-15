8ed54efdea54b9345a1ad57bce61813d6bb24b28
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-15 12:54:25 +0000

    May 15, 2024, 6:24 PM

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const randomNumber1 = Math.floor(Math.random() * 6) + 1
 const randomDiceImage = 'dice' + randomNumber1 + '.png'
 const randomImageSource = './images/' + randomDiceImage
+
+let image1 = document.querySelectorAll('img')[0]
+image1.setAttribute('src', randomImageSource)
