dffd60d5e40298cd3045028388383d72bec09a20
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-15 12:38:25 +0000

    May 15, 2024, 6:08 PM

--- index.js
+++
@@ -1,8 +0,0 @@
-const randomNumber = Math.round(Math.random() * 6) + 1
-
-const getImage = function (random) {
-    const image = document.querySelector('img').setAttribute('src', random)
-    console.log(image)
-}
-
-getImage(randomNumber)
