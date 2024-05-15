6ca2e44181c8a192843de3848366e4817608d696
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-15 11:59:11 +0000

    May 15, 2024, 5:29 PM

--- index.js
+++
@@ -1,6 +0,0 @@
-const heading = document.querySelector('h1').classList.add('huge')
-
-const anchorTag = document
-    .querySelector('a')
-    .setAttribute('href', 'https://www.bing.com')
-console.log(anchorTag)
