d2d55eed2171f12126be9d528426b1f6260f36b4
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-15 15:52:43 +0000

    May 15, 2024, 9:22 PM

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const numberOfDrumButtons = document.querySelectorAll('.button').length
+const numberOfDrumButtons = document.querySelectorAll('.drum').length
 
 for (let i = 0; i < numberOfDrumButtons; i++) {
     document
-        .querySelectorAll('.button')
+        .querySelectorAll('.drum')
         [i].addEventListener('click', function () {
             alert('button got clicked')
         })
