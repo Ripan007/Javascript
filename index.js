5c21270a5f8c2da3b569d15eeda69300f68fbcb3
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-16 06:00:56 +0000

    May 16, 2024, 11:30 AM

--- index.js
+++ index.js
@@ -7,32 +7,32 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
             const buttonInnerHtml = this.innerHTML
             switch (buttonInnerHtml) {
                 case 'w':
-                    let tom1 = new Audio('sounds/tom-1.mp3')
+                    const tom1 = new Audio('sounds/tom-1.mp3')
                     tom1.play()
                     break
                 case 'a':
-                    let tom2 = new Audio('sounds/tom-2.mp3')
-                    tom1.play()
+                    const tom2 = new Audio('sounds/tom-2.mp3')
+                    tom2.play()
                     break
                 case 's':
-                    let tom3 = new Audio('sounds/tom-3.mp3')
-                    tom1.play()
+                    const tom3 = new Audio('sounds/tom-3.mp3')
+                    tom3.play()
                     break
                 case 'd':
-                    let tom4 = new Audio('sounds/tom-4.mp3')
-                    tom1.play()
+                    const tom4 = new Audio('sounds/tom-4.mp3')
+                    tom4.play()
                     break
                 case 'j':
-                    let crash = new Audio('sounds/crash.mp3')
-                    tom1.play()
+                    const crash = new Audio('sounds/crash.mp3')
+                    crash.play()
                     break
                 case 'k':
-                    let kick = new Audio('sounds/kick-bass.mp3')
-                    tom1.play()
+                    const kick = new Audio('sounds/kick-bass.mp3')
+                    kick.play()
                     break
                 case 'l':
-                    let snare = new Audio('sounds/snare.mp3')
-                    tom1.play()
+                    const snare = new Audio('sounds/snare.mp3')
+                    snare.play()
                     break
 
                 default:
