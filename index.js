4f0561b9bc68ea4edd465172de05c77917b3e189
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-16 05:57:07 +0000

    May 16, 2024, 11:27 AM

--- index.js
+++ index.js
@@ -6,8 +6,32 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
         [i].addEventListener('click', function () {
             const buttonInnerHtml = this.innerHTML
             switch (buttonInnerHtml) {
-                case w:
-                    let tom1 = new Audio('./sounds/tom1.mp3')
+                case 'w':
+                    let tom1 = new Audio('sounds/tom-1.mp3')
+                    tom1.play()
+                    break
+                case 'a':
+                    let tom2 = new Audio('sounds/tom-2.mp3')
+                    tom1.play()
+                    break
+                case 's':
+                    let tom3 = new Audio('sounds/tom-3.mp3')
+                    tom1.play()
+                    break
+                case 'd':
+                    let tom4 = new Audio('sounds/tom-4.mp3')
+                    tom1.play()
+                    break
+                case 'j':
+                    let crash = new Audio('sounds/crash.mp3')
+                    tom1.play()
+                    break
+                case 'k':
+                    let kick = new Audio('sounds/kick-bass.mp3')
+                    tom1.play()
+                    break
+                case 'l':
+                    let snare = new Audio('sounds/snare.mp3')
                     tom1.play()
                     break
 
