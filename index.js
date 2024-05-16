3b873b4883d44e7254e53af42ebbcde649c299b3
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-16 05:41:53 +0000

    May 16, 2024, 11:11 AM

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
     document
         .querySelectorAll('.drum')
         [i].addEventListener('click', function () {
-            console.log((this.textContent.style.color = 'red'))
+            this.style.color = 'red'
         })
 }
 
