13236a5ebf8a470becb1cf0cd4eade7625d008c8
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-16 05:50:16 +0000

    May 16, 2024, 11:20 AM

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
     document
         .querySelectorAll('.drum')
         [i].addEventListener('click', function () {
-           const button
+            const buttonInnerHtml = this.innerHTML
+            switch (buttonInnerHtml) {
+                case w:
+                    let tom1: break
+
+                default:
+                    break
+            }
         })
 }
 
