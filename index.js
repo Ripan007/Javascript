6af1f90bb496103787a281325526ea71cd717018
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 13:06:45 +0000

    May 17, 2024, 6:36 PM

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// primitive
-let age = 10
-let oldAge = age
-age = 20
-console.log(`age is ${age}`)
-console.log(`old age is ${oldAge}`)
-
-// non primitive
-const me = {
-    name: 'john',
-    age: 10,
+const jessica = {
+    name: 'jessica',
 }
-
-const friend = me
-me.age = 20
-console.log(`object me age is ${me.age}`)
-console.log(`object friend age is ${friend.age}`)
