cb5f9fb2183a15931811f7943cb0219987af5739
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 06:14:30 +0000

    May 17, 2024, 11:44 AM

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+/* Write your code below. Good luck! 🙂 */
+
+const mark = {
+    fullName: 'mark',
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / (this.height * this.height)
+        return this.bmi
+    },
+}
+
+const john = {
+    fullName: 'john',
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = mass / (height * height)
+        return this.bmi
+    },
+}
+
+const markBmi = mark.calcBMI()
+console.log(markBmi)
