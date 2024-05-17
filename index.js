ce99c6f43d8287f5c24b74db2c417a6ad76aa22a
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 11:29:33 +0000

    May 17, 2024, 4:59 PM

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+function calAge(birthyear) {
+    const age = 2024 - birthyear
+    console.log(firstName)
+    return age
+}
+
+const firstName = 'ripan'
+
+console.log(calAge(1991))
