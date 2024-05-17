64c63fe40bf22a466f8251e73834a1dda071c4aa
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 07:14:23 +0000

    May 17, 2024, 12:44 PM

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function calAge(birthyear) {
-    console.log(`name is ${firstName}`)
+    // console.log(`name is ${firstName}`)
     const age = 2024 - birthyear
+    function printAge() {
+        const output =
+    }
     return age
 }
 
@@ -8,4 +11,4 @@ const firstName = 'ripan'
 
 calAge()
 
-//  you are 46 , born in
+//  you are 46 , born in 1991
