46f1e0456fa78f6b7e9a4e50c8556bcc41e03f71
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 04:37:55 +0000

    May 17, 2024, 10:07 AM

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ const ripan = {
 
 console.log(ripan.friend[ripan.friend.length - 2])
 
-//  because of non focus , discontunity i   could not able to achieve wha
+//  because of non focus , discontunity i   could not able to achieve what i want ,  but from know onwards i will not do  the  same mistake ones again and i will sort out all thosse issue  and become what i want  to become
+
+
+?
