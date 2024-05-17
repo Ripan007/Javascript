ab94e917c1e77240420247a9bb782f457f87e6f7
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 06:55:59 +0000

    May 17, 2024, 12:25 PM

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-// "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
-/* Write your code below. Good luck! 🙂 */
+const myName = 'ripan'
 
-const mark = {
-    fullName: 'mark miller',
-    mass: 78,
-    height: 1.69,
-    calcBMI: function () {
-        this.bmi = this.mass / (this.height * this.height)
-        return this.bmi
-    },
+function first() {
+    const age = 30
+    if (age >= 30) {
+        const decade = 3
+        let millenial = true
+    }
+
+    function second() {
+        const job = 'teacher'
+        console.log(`${myName} is a ${age}-old `)
+    }
+    second()
 }
-
-const john = {
-    fullName: 'john smith',
-    mass: 92,
-    height: 1.95,
-    calcBMI: function () {
-        this.bmi = this.mass / (this.height * this.height)
-        return this.bmi
-    },
-}
-
-const markBmi = Math.floor(mark.calcBMI())
-const johnBmi = Math.floor(john.calcBMI())
-console.log(markBmi, johnBmi)
-
-if (markBmi > johnBmi) {
-    console.log(`${mark.fullName}' BMI  (${markBmi})} is higher than ${john.fullName} (${johnBmi})
-}`)
-} else if (johnBmi > markBmi) {
-    console.log(`${john.fullName}' BMI  (${johnBmi})} is higher than ${mark.fullName} (${markBmi})
-}`)
-} else {
-    console.log('no own wins')
-}
+first()
