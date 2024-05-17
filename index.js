dc32166b2bcf01c2462fdae155a6c5a244b0c8be
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 12:47:48 +0000

    May 17, 2024, 6:17 PM

---
+++ index.js
@@ -0,0 +1,12 @@
+// primitive
+let age = 10
+let oldAge = age
+age = 20
+console.log(`age is ${age}`)
+console.log(`old age is ${oldAge}`)
+
+// non primitive
+const me = {
+    name: 'john',
+    age: 10,
+}
