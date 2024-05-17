5750bfb600a77bb37603a38f4d417848001310ed
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 04:31:19 +0000

    May 17, 2024, 10:01 AM

--- index.js
+++ index.js
@@ -8,18 +8,4 @@ const ripan = {
     friend: ['raju', 'gopi', 'rajiv'],
 }
 
-const interestedIn = prompt(
-    'what you want  to interested to know about ripan , about is work  life  and more'
-)
-
-if (ripan[interestedIn]) {
-    console.log(`show the outcome ${ripan[interestedIn]}`)
-} else {
-    console.log(
-        'choose from the above  data ,choose the  valid option which are available'
-    )
-}
-
-ripan.loaction = 'portblair'
-ripan.twitter = '@ripan01'
-console.log(ripan)
+console.log(ripan.friend[ripan.friend.length - 2])
