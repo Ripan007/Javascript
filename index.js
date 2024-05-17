439343062be24e45a1d35355b5e30ea5c86d42e2
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 04:40:23 +0000

    May 17, 2024, 10:10 AM

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// focus ,avoid distractions , dream
-
 const ripan = {
     firstName: 'ripan',
     lastName: 'singh',
@@ -7,10 +5,3 @@ const ripan = {
     job: 'programmer',
     friend: ['raju', 'gopi', 'rajiv'],
 }
-
-console.log(ripan.friend[ripan.friend.length - 2])
-
-//  because of non focus , discontunity i   could not able to achieve what i want ,  but from know onwards i will not do  the  same mistake ones again and i will sort out all thosse issue  and become what i want  to become
-
-
-?
