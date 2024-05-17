fe252deb14e6c407a43ee746321719ed1a037a1b
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 05:05:12 +0000

    May 17, 2024, 10:35 AM

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const ripan = {
     firstName: 'ripan',
     lastName: 'mondal',
-    birthYear: 1995,
+    birthYear: 1999,
     job: 'programmer',
+    friends: ['first', 'second', 'third'],
     hasDriverLicense: true,
+    calAge: function (birthYear) {
+        return 2024 - birthYear
+    },
 }
+
+console.log(ripan.calAge(1995))
