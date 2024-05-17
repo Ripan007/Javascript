5ee63d5d4bd66ed5cb7d022f4d11f811a5beab4e
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 12:11:22 +0000

    May 17, 2024, 5:41 PM

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const jonas = {}
+const jonas = {
+    name: 'jonas',
+    year: 1995,
+    calAge: function () {
+        const age = 2024 - this.year
+        return age
+    },
+}
+
+console.log(jonas.calAge())
