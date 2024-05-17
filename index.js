320a0a48866c9c75cbfefd62f8f0abe3eb73959d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-17 16:33:25 +0000

    May 17, 2024, 10:03 PM

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const restaurant = {
     categories: ['indian', 'chinease', 'italian'],
     starterMenu: ['dal', 'rice', 'vegggie'],
     mainMenu: ['chicken', 'fish'],
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], [this.mainMenu[mainIndex]]]
+    },
 }
+
+const [starterCourse, mainCourse] = restaurant.order(0, 1)
+console.log(starterCourse)
+console.log(mainCourse)
