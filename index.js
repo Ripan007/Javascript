b87e74bc7b083376125fa8749215fdb4c6900fbf
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-18 02:19:58 +0000

    May 18, 2024, 7:49 AM

--- index.js
+++ index.js
@@ -1,53 +1,40 @@
-'use strict';
-const restaurant = {
-    name: 'bhaba  ka  dhaba',
-    location: 'delhi chourha',
-    categories: ['indian', 'chinease', 'italian'],
-    starterMenu: ['dal', 'rice', 'vegie'],
-    mainMenu: ['chicken', 'fish'],
-    openingHours: {
-        mon: {
-            open: 9,
-            close: 7,
-        },
-        thu: {
-            open: 10,
-            close: 9,
-        },
-        sat: {
-            open: 2,
-            close: 9,
-        },
-    },
-    order: function (starterIndex, mainIndex) {
-        return [this.starterMenu[starterIndex], [this.mainMenu[mainIndex]]];
-    },
-    orderDelivery: function ({ starterIndex = 1, mainIndex, time, address }) {
-        console.log(this);
-        console.log(
-            `ordered delivered was ${this.starterMenu[starterIndex]}, ${this.mainMenu[mainIndex]} at ${time} and the location was ${address}`
-        );
-    },
-    orderPasta: function (ing1, ing2, ing3) {
-        console.log(`pasta with ingredient ${ing1},${ing2},${ing3}`);
-    },
-    orderPizza: function (mainIngredients, ...restIngredients) {
-        console.log('main', mainIngredients);
-        console.log('rest', restIngredients);
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
     },
 };
-
-const res1 = {
-    name: 'raju',
-    numOfGuest: 10,
-};
-
-const res2 = {
-    name: 'dhaba',
-    owner: 'rani',
-};
-
-res1.owner &&= '<ANONYMOUS>';
-res2.owner &&= '<ANONYMOUS>';
-console.log(res1);
-console.log(res2);
