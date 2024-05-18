4ea0c964b662c2155acb6729b97081e685ae2832
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-18 04:47:18 +0000

    May 18, 2024, 10:17 AM

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const weekdays = ['mon', 'sun'];
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
-    [weekdays[0]]: {
+    [weekdays[1]]: {
         open: 12,
         close: 2,
     },
@@ -15,8 +15,9 @@ const restaurant = {
     },
 };
 
-const days = ['mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'sun'];
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 for (const day of days) {
+    console.log('day', day);
     const open = restaurant.openingHours[day]?.open;
     console.log(`on ${day},we open at ${open}`);
 }
