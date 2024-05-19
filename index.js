3713266a25ce966db323bcf47875299cd71711e6
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 11:59:47 +0000

    May 19, 2024, 5:29 PM

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const oneWord = function (str) {
-    return str.replace(/ /g, '').toLowerCase();
+const greet = function (greeting) {
+    return function (name) {
+        console.log(`${greeting} ${name}`);
+    };
 };
 
-const upperFirstWord = function (str) {
-    const [first, ...others] = str.split('');
-    return [first.toUpperCase(), ...others].join('');
-};
-
-// higher order functions
-
-const transformer = function (str, fn) {
-    console.log(`original string : ${str}`);
-    console.log(`transformed string : ${fn(str)}`);
-    console.log(`transformed  by : ${fn.name}`);
-};
-
-transformer('javascript is the best ', upperFirstWord);
-transformer('javascript is the best', oneWord);
+const heyGreet = greet('hi');
+heyGreet('mohit');
