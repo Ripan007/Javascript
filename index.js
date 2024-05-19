172010829ac10cced94350952425b0be4d2799c5
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 02:21:47 +0000

    May 19, 2024, 7:51 AM

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const rest = new Map();
-rest.set('name', 'dhabha');
-rest.set(1, 'france ,itaily');
-rest.set(2, 'portugal, port');
-
-console.log(rest.set(3, 'vvv,jjj'));
+const gameEvents = new Map([[17, ' 🥅 GOAL'], []]);
