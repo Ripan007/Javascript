968f9aedf3e40cdbafe7f09e9b87780e89e093f8
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 05:09:50 +0000

    May 19, 2024, 10:39 AM

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const capitalizeName = function (name) {
-    const names = name.split(' ');
+    const names = name.split('');
     const nameUpper = [];
-    for (const n of names) {
+
+    for (let n of names) {
         nameUpper.push(n[0].toUpperCase() + n.slice(1));
     }
-    console.log(nameUpper.join(' '));
+    console.log(nameUpper.join(''));
 };
 
-capitalizeName('jessica and smith devis');
-capitalizeName('jonas schmedman');
+capitalizeName('ripan is awesome');
