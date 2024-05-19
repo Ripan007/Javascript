58a7de58c81c9419c76ca09b4c5558ef8cff9a20
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 09:55:44 +0000

    May 19, 2024, 3:25 PM

--- index.js
+++ index.js
@@ -9,5 +9,20 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(rows);
     for (const row of rows) {
         const [first, second] = row.toLowerCase().trim().split('_');
+        const output = `${first} ${second.replace(
+            second[0],
+            second[0].toUpperCase()
+        )}`;
+        console.log(output);
     }
 });
+
+/*
+underscore_case
+first_name
+Some_Variable
+calculate_AGE
+delayed_depature
+
+
+*/
