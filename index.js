ffb9e7fb99554352184998101a8b42e25c084e82
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 11:04:03 +0000

    May 19, 2024, 4:34 PM

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ const jonas = {
 const checkIn = function (flightNum, passenger) {
     flightNum = 'HTT77';
     passenger.name = 'mr.' + passenger.name;
-    if (passenger.password === 2223344) {
+    if (passenger.password === 22233444) {
         alert('password correct');
     } else {
         console.log('fill up correct credentials');
     }
 };
 
+const newPassword = function (person) {
+    person.password = Math.trunc(Math.random() * 100000000000);
+};
+newPassword(jonas);
 checkIn(flight, jonas);
-console.log(`flight name is : ${flight}`);
-console.log(jonas);
