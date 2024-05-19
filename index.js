b9548c559b1d499b712362c27095841470c92afa
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 04:23:05 +0000

    May 19, 2024, 9:53 AM

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-const airplane = 'tap air portugal';
-const plane = 'a320';
+const passenger = 'JOnas';
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect =
+    passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(`passengerCorrect data :${passengerCorrect}`);
