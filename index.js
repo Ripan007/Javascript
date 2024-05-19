cd7d30b1237da119acdac9d9ad14bba9e81306bc
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 09:26:23 +0000

    May 19, 2024, 2:56 PM

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
+
+const text = document.querySelector('textarea').value;
+console.log(text);
