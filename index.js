f439208e2c1f1232ead0d65a3721aa8ec7595b49
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 11:28:10 +0000

    May 19, 2024, 4:58 PM

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const oneWord = function (str) {
 
 const upperFirstWord = function (str) {
     const [first, ...others] = str.split('');
-    return [first.toUpperCase(), ...others].join();
+    return [first.toUpperCase(), ...others].join('');
 };
 
 // higher order functions
 
 const transformer = function (str, fn) {
     console.log(`original string : ${str}`);
-    console.log(`transformed string ${fn(str)}`);
-    console.log('');
+    console.log(`transformed string : ${fn(str)}`);
+    console.log(`transformed  by : ${fn.name}`);
 };
+
+transformer('javascript is the best ', upperFirstWord);
+transformer('javascript is the best', oneWord);
