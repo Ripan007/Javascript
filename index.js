60674f9bc00891fe1364463211560a97252198e8
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 09:43:43 +0000

    May 19, 2024, 3:13 PM

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ document.body.append(document.createElement('button'));
 
 const text = document.querySelector('textarea').value;
 
-document.querySelector('button');
+document.querySelector('button').addEventListener('click', function () {
+    console.log('button got clicked');
+});
