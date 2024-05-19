05fe9836ff7e7d48cec7fc10400666611938969d
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 11:25:24 +0000

    May 19, 2024, 4:55 PM

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const upperFirstWord = function (str) {
     const [first, ...others] = str.split('');
     return [first.toUpperCase(), ...others].join();
 };
+
+// higher order functions
+
+const transformer = function (str, fn) {
+    console.log(`original string : ${str}`);
+    console.log(`transformed string ${fn(str)}`);
+    console.log('');
+};
