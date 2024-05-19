ef296e4eca7532e33ef809570d1e123f2d9d8041
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 05:06:11 +0000

    May 19, 2024, 10:36 AM

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const capitalizeName = function (name) {
     const names = name.split(' ');
     const nameUpper = [];
     for (const n of names) {
+        nameUpper.push(n[0].toUpperCase() + n.slice(1));
     }
+    console.log(nameUpper.join(' '));
 };
 
 capitalizeName('jessica and smith devis');
