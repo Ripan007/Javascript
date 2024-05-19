d297963ae51f534b5050196750561ccbba244bba
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 09:58:14 +0000

    May 19, 2024, 3:28 PM

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ const text = document.querySelector('textarea').value;
 document.querySelector('button').addEventListener('click', function () {
     const text = document.querySelector('textarea').value;
     const rows = text.split('\n');
-    console.log(rows);
+    // console.log(rows);
     for (const row of rows) {
         const [first, second] = row.toLowerCase().trim().split('_');
+        console.log(row, first, second);
         const output = `${first} ${second.replace(
             second[0],
             second[0].toUpperCase()
         )}`;
-        console.log(output);
+        // console.log(output);
     }
 });
 
