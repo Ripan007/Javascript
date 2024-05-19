9dcb211f9d151885b6994328958c2c3451ed4ebb
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 04:35:01 +0000

    May 19, 2024, 10:05 AM

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const email = 'jonas@gmail.io';
-const loginEmail = '    Jonas@gmail.Io \n ';
-
-const correctEmail = loginEmail.toLocaleLowerCase().trim();
-
-console.log(email === correctEmail);
+const priceGb = '';
