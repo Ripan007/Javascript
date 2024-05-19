d87509742a5fb3263e8ba6f4f174bb7524404e3c
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 10:23:26 +0000

    May 19, 2024, 3:53 PM

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 
+const bookings = [];
 const createBooking = function (flightNum, numPassenger, price) {
     const booking = {
         flightNum,
         numPassenger,
         price,
     };
+    console.log(booking);
+    bookings.push(booking);
 };
-createBooking();
+createBooking('gg42');
