abd1ebb2ecf6876f9a8b1b398c84630705133b18
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 13:24:35 +0000

    May 19, 2024, 6:54 PM

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const vistra = {
-    airline: 'vistara',
-    iatacode: 'yy5',
-    bookings: [],
-    book: function (flightNum, name) {
-        console.log(
-            `${name} booked a seat on ${this.airline} flight ${this.iatacode} ${flightNum}`
-        );
-    },
-};
-
-vistra.book(66, 'mohit');
+// asynchronous javascript //
