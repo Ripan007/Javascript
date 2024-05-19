a3afefcb1dd1b7ef7c156c7843f824da02ee055f
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 11:23:45 +0000

    May 19, 2024, 4:53 PM

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-'use strict';
-
-const flight = 'uu34';
-const jonas = {
-    name: 'john schenwin',
-    password: 22233444,
+const oneWord = function (str) {
+    return str.replace(/ /g, '').toLowerCase();
 };
 
-const checkIn = function (flightNum, passenger) {
-    flightNum = 'HTT77';
-    passenger.name = 'mr.' + passenger.name;
-    if (passenger.password === 22233444) {
-        alert('password correct');
-    } else {
-        console.log('fill up correct credentials');
-    }
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split('');
+    return [first.toUpperCase(), ...others].join();
 };
-
-const newPassword = function (person) {
-    person.password = Math.trunc(Math.random() * 100000000000);
-};
-newPassword(jonas);
-checkIn(flight, jonas);
