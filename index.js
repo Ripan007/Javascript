48b44f3e125ae0f0ae843da7164aa5c893ac9602
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 04:47:18 +0000

    May 19, 2024, 10:17 AM

---
+++ index.js
@@ -0,0 +1,11 @@
+const checkBaggage = function (items) {
+    const baggage = items.toLowerCase();
+    if (baggage.includes('knife') && baggage.includes('gun')) {
+        console.log('you are not allowed to enter');
+    } else {
+        console.log('you are allowed to enter');
+    }
+};
+
+checkBaggage('i have a knife ');
+checkBaggage('i have  a gun');
