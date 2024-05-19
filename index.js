fe7677942aeb32fcddf207b1d2edeb219413b1f1
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 03:55:09 +0000

    May 19, 2024, 9:25 AM

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 const airplane = 'tap air portugal';
 const plane = 'a320';
-
-console.log(airplane.indexOf('r'));
-console.log(airplane.lastIndexOf('r'));
-
-console.log(airplane.indexOf('portugals'));
