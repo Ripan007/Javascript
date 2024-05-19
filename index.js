b58e1aaf7bc3415c865f950fea24c0fd598ccf7c
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 10:30:22 +0000

    May 19, 2024, 4:00 PM

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //  default parameter
 
 const bookings = [];
-const createBooking = function (flightNum, numPassenger = 90, price = 6000) {
+const createBooking = function (flightNum, numPassenger, price = 6000) {
     const booking = {
         flightNum,
         numPassenger,
@@ -14,5 +14,5 @@ const createBooking = function (flightNum, numPassenger = 90, price = 6000) {
 };
 
 createBooking('bbf', 200, 2000);
-createBooking('kkk');
+createBooking('lll');
 createBooking('kkk', undefined, 1000);
