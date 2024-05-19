cabbf782f599905ed50a5b6377402c55bf240769
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 13:07:00 +0000

    May 19, 2024, 6:37 PM

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const vistra = {
     iatacode: 'yy5',
     bookings: [],
     book: function (flightNum, name) {
-        console.log(`${name} booked a seat on ${flightNum} flight `);
+        console.log(
+            `${name} booked a seat on ${flightNum} flight ${this.iatacode} ${flightNum}`
+        );
     },
 };
+
+vistra.book('jj1', 'mohit');
