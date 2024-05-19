53de8f8d1e4e79049c524d64e01a841b767ca875
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-19 10:27:30 +0000

    May 19, 2024, 3:57 PM

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 'use strict';
 
-const bookings = [];
-const createBooking = function (flightNum, numPassenger, price) {
-    const booking = {
-        flightNum,
-        numPassenger,
-        price,
-    };
-    console.log(booking);
-    bookings.push(booking);
-};
-createBooking('gg42');
+//  default parameter
+
+const createBooking = function(flightNum,numPassenger,)
