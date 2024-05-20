64233da11cc1077579dd1305f0b7c785de67c9f2
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-20 10:24:50 +0000

    May 20, 2024, 3:54 PM

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-console.log('test start');
-setTimeout(() => {
-    console.log('0 second timer');
-}, 0);
-Promise.resolve('resolved promise 1').then(res => console.log(res));
-console.log('test end');
+console.log('welcome');
