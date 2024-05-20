2125939db7304ca269f7ccee8677590930dc4ae2
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-20 03:39:23 +0000

    May 20, 2024, 9:09 AM

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v2/all');
+request.open('GET', 'https://restcountries.com/v2/name/portugal');
 request.send();
 
 request.addEventListener('load', function () {
-    const data = JSON.parse(request.responseText);
+    const [data] = JSON.parse(request.responseText);
     console.log(data);
 });
