06222de1513e93f7d0af0fcfca9eb58bfe3e540a
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-20 05:15:01 +0000

    May 20, 2024, 10:45 AM

--- index.js
+++ index.js
@@ -76,7 +76,11 @@ const renderCountry = function (data, className = '') {
 const getCountryData = function (country) {
     fetch(`https://restcountries.com/v2/name/${country}`)
         .then(response => response.json())
-        .then(data => renderCountry(data[0]));
+        .then(data => {
+            renderCountry[data[0]];
+            const neighbour = data[0].borders[0];
+            if (!neighbour) return;
+        });
 };
 
 getCountryData('bharat');
