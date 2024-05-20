c2bdc84d5d15d39eca0292cb139bd3928da14186
CodeExcite <92440897+Ripan007@users.noreply.github.com>
2024-05-20 05:23:57 +0000

    May 20, 2024, 10:53 AM

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ const getCountryData = function (country) {
     fetch(`https://restcountries.com/v2/name/${country}`)
         .then(response => response.json())
         .then(data => {
-            renderCountry[data[0]];
+            renderCountry(data[0]);
             const neighbour = data[0].borders[0];
             if (!neighbour) return;
             //  fetch 2 country
@@ -87,4 +87,4 @@ const getCountryData = function (country) {
         .then(data => renderCountry(data, 'neighbour'));
 };
 
-getCountryData('portugal');
+getCountryData('bharat');
